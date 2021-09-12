3728883f20d2fa8392cc31161651f856af82cf92
Kevin Hookumchand <khookumchand@gmail.com>
2021-09-12 12:39:53 -0400

    Added to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'body_mass_indices#index'
   resources :body_mass_indices
   resources :three_day_workouts
   resources :five_day_workouts
