9cbf9ee99bdcd66da522e34f250645f36a754f0d
Kevin Hookumchand <khookumchand@gmail.com>
2021-09-15 21:15:12 -0400

    Testing API

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,17 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+fetch("https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=150&height=68", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "body-mass-index-bmi-calculator.p.rapidapi.com",
+		"x-rapidapi-key": "6f8998296dmsh8a7269c19e83760p1a8787jsncdaeb262681c"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
