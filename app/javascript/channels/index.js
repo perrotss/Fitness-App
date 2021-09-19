4531024dec15106c387c44af6e1b6d1b1037ddea
Kevin Hookumchand <khookumchand@gmail.com>
2021-09-19 17:15:20 -0400

    Working on API fetch and display

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -4,16 +4,40 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-fetch("https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=150&height=68&rapidapi-key=6f8998296dmsh8a7269c19e83760p1a8787jsncdaeb262681c", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "body-mass-index-bmi-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "6f8998296dmsh8a7269c19e83760p1a8787jsncdaeb262681c"
+let bmi = {
+	API_KEY: '6f8998296dmsh8a7269c19e83760p1a8787jsncdaeb262681c',
+	fetchBMI: function (height, weight) {
+		fetch(
+			"https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=" +
+			weight +
+			"&height=" +
+			height +
+			"&" +
+			this.API_KEY
+		)
+    .then((response) => response.json())
+    .then((data) => this.displayBMI(data));
+  },
+	displayBMI: function(data) {
+		const { weight } = body.weight;
+		const { height } = body.height;
+		document.querySelector(".")
 	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+}
+
+
+
+
+// fetch("https://body-mass-index-bmi-calculator.p.rapidapi.com/imperial?weight=150&height=68&rapidapi-key=6f8998296dmsh8a7269c19e83760p1a8787jsncdaeb262681c", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "body-mass-index-bmi-calculator.p.rapidapi.com",
+// 		"x-rapidapi-key": "6f8998296dmsh8a7269c19e83760p1a8787jsncdaeb262681c"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
