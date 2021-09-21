b86749be94510faeb7c740a4b73606b2d500d229
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2021-09-20 23:54:12 -0400

    validation and methods to controller

--- app/controllers/body_mass_indices_controller.rb
+++ app/controllers/body_mass_indices_controller.rb
@@ -1,5 +1,20 @@
 class BodyMassIndicesController < ApplicationController
+  before_action :set_bmi, only %i[create new]
+  
   def index
     @bmi = BodyMassIndex.new
+  end
+
+  def create
+    raise
+  end
+
+  private
+  def set_bmi
+    @bmi = BodyMassIndex.find(params[:id])
+  end
+  
+  def bmi_params
+    params.require(:bmi).permit(:feet, :inches, :pounds)
   end
 end
--- app/models/body_mass_index.rb
+++ app/models/body_mass_index.rb
@@ -1,2 +1,5 @@
 class BodyMassIndex < ApplicationRecord
+    validates :feet, presence: true
+    validates :inches, presence: true
+    validates :pounds, presence: true
 end
