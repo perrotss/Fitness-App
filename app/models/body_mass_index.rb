787016da741e7a52bde4e207828f1d7b80fe0d28
Francesco Perrozzi <francescoperrozzi3@gmail.com>
2021-09-22 22:22:21 -0400

    api call now working

--- app/models/body_mass_index.rb
+++ app/models/body_mass_index.rb
@@ -2,4 +2,7 @@ class BodyMassIndex < ApplicationRecord
     validates :feet, presence: true
     validates :inches, presence: true
     validates :pounds, presence: true
+    validates :feet, numericality: { only_integer: true }
+    validates :inches, numericality: { only_integer: true }
+    validates :pounds, numericality: { only_integer: true }
 end
