d27ad41469207d705bc3c654ad0a29741aec9e6e
Kevin Hookumchand <khookumchand@gmail.com>
2021-10-03 14:35:59 -0400

    Created about page, route, controller method and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  get '/about', to: 'home#about'
   resources :body_mass_indices
   resources :three_day_workouts
   resources :five_day_workouts
